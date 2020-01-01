ab5f27a743028dc230bb1567ea73a85a2d730b9a
Carlos Quinones <cquinones100@gmail.com>
2019-12-31 23:40:37 -0500

    add bio

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(function(req, res, next) {
+  res.cookie('session', 'hi');
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  next();
 });
 
-app.listen(PORT, () => console.log('hit the port'));
+app.get('/', (req, res) => {
+  res.send('Hello World! v2')
+});
+
+app.listen(PORT);
--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
-  return  <div>Carlos Quinones</div>;
- }
+const App = () => (
+  <div>
+    <h1>Carlos Quinones</h1>
+    <p>I'm an Engineering Lead at Knotch in NYC. I love to write music and ride my bike.</p>
+  </div>
+);
 
 export default App;
